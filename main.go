3d1193313f63ebd372a05e7f8b89ff34a33341e4
azzomty <medorya35@gmail.com>
2026-08-11 02:06:59 +0300

    Force new session by renaming whatsapp.db to whatsapp_v2.db

--- main.go
+++ main.go
@@ -447,7 +447,7 @@ func main() {
 	store.LoadAll(".")
 
 	dbLog := waLog.Stdout("Database", "ERROR", true)
-	container, err := sqlstore.New(context.Background(), "sqlite3", "file:whatsapp.db?_foreign_keys=on", dbLog)
+	container, err := sqlstore.New(context.Background(), "sqlite3", "file:whatsapp_v2.db?_foreign_keys=on", dbLog)
 	if err != nil {
 		panic(err)
 	}
