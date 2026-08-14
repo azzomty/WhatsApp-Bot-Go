031c02d91fe4949e1871899322bbf03aee04883a
azzomty <medorya35@gmail.com>
2026-08-14 23:33:24 +0300

    Remove test files

--- test_pin_gif.go
+++
@@ -1,9 +0,0 @@
-package main
-import (
-	"fmt"
-	"whatsapp-bot/internal/pinterest"
-)
-func main() {
-	res := pinterest.SearchPinterestMedia("anime", ".gif")
-	fmt.Println("GIF Results:", len(res))
-}
--- test_pin_search.go
+++
@@ -1,9 +0,0 @@
-package main
-import (
-	"fmt"
-	"whatsapp-bot/internal/pinterest"
-)
-func main() {
-	res := pinterest.SearchPinterest("anime", "all")
-	fmt.Println("Initial Pins:", len(res))
-}
