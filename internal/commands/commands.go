d6d9d40cb52c1d97e015f149cc75d296513b7622
azzomty <medorya35@gmail.com>
2026-09-20 19:49:06 +0300

    Restore to temp commit 5bfc377

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -198,9 +198,6 @@ func Handle(ctx *BotContext) {
 	if HandleMDMCommand(ctx) {
 		return
 	}
-	if HandleFontImageCommand(ctx) {
-		return
-	}
 	if HandleDownloadCommand(ctx) {
 		return
 	}
@@ -1409,10 +1406,6 @@ func showCommands(ctx *BotContext) {
 .فوريو (صور عشوائية)
 .تطقيم (صور متطابقة)
 
-[ أوامر الخطوط والزخرفة ]
-	.قائمة الخطوط (لعرض وتطبيق أكثر من 120 زخرفة نصوص يونيكود)
-	.خط [النص] | [اسم الخط] | [لون الخلفية] | [لون النص] (لصنع ملصق شفاف بخطوط حقيقية فخمة مثل Creattion Demo)
-
 [ أوامر الملصقات ]
 .ملصق (لتحويل الصور والفيديوهات لملصقات)
 .عمل حزمة (لتحويل مجموعة صور/فيديوهات إلى حزمة ملصقات)
@@ -2581,8 +2574,8 @@ func HandleSyrian(ctx *BotContext) {
 func HandleFontsCommand(ctx *BotContext) bool {
 	text := strings.TrimSpace(ctx.Text)
 	if text == ".قائمة الخطوط" || text == ".fonts list" {
-		sampleTextEn := "I LOVE ASSUITY i love assuity"
-		sampleTextAr := "احب اسيوتي"
+		sampleTextEn := "Abc 123"
+		sampleTextAr := "تجربة"
 
 		var msgs []string
 		var currentMsg strings.Builder
